0a47934aa9897713c0e74c1e4983cb058eb006e3
IMH51 <ianmartinharrison@outlook.com>
2019-01-30 20:01:46 +0000

    build react animal shelter app

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -4,7 +4,12 @@ import Pet from './Pet'
 
 class PetBrowser extends React.Component {
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    return (<div className="ui cards">{this.props.pets.map(petObject => {
+        return (<Pet pet={petObject} key={petObject.id} onAdoptPet={this.props.onAdoptPet}/>)
+        }
+      )
+    }
+  </div>)
   }
 }
 
