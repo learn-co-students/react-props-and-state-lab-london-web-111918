94aa95ccab5209b31b86fbd827a6dd24227b1023
jodyneckles <jody@technative.tv>
2019-02-01 09:08:32 +0000

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import Filters from './Filters'
 import PetBrowser from './PetBrowser'
 
 class App extends React.Component {
-  constructor() {
+  constructor () {
     super()
 
     this.state = {
@@ -15,19 +15,62 @@ class App extends React.Component {
     }
   }
 
-  render() {
+  changeFilter = (event) => {
+    this.setState({ filters: { type: event.target.value } })
+  }
+
+  findPets = () => {
+    const { type } = this.state.filters
+    const url = type === 'all'
+      ? '/api/pets'
+      : `/api/pets?type=${type}`
+    fetch(url)
+      .then(res => res.json())
+      .then(pets => this.setState({ pets }))
+  }
+
+  // adoptPet = id => {
+  //   const { pets } = this.state
+  //   const pet = pets.find(pet => pet.id === id)
+  //   pet.isAdopted = true
+  //   this.setState({}) // the above code mutates the pet (by reference) but state is not rerendered. By calling state with an empty object it causes a rerender!!!!!
+  // }
+
+  // adoptPet = (id) => {
+  //   const pets = [...this.state.pets] // we copy the original array
+  //   const foundPet = pets.find(pet => pet.id === id)
+  //   const foundPetCopy = JSON.parse(JSON.stringify(foundPet)) // convert object to a string and back to an object again to make a new DEEP CLONE object. This can be mutated without affecting state. YOU CANNOT COPY functions as JSON cannot copy functions.
+  //   foundPetCopy.isAdopted = true
+  //   const indexToSwap = pets.indexOf(foundPet)
+  //   pets[indexToSwap] = foundPetCopy
+  //   this.setState({ pets }) // there are libraries like IMMUTATBLEJS that can help with this long code.
+  // }
+
+  adoptPet = (id) => {
+    const pets = this.state.pets.map(pet =>
+      pet.id === id
+        ? { ...pet, isAdopted: true } // this creates a NEW object via shallow copy
+        : pet
+    )
+    this.setState({ pets })
+  }
+
+  render () {
+    const { changeFilter, findPets, adoptPet } = this
+    const { pets } = this.state
+    // change this class into a function component all you have to do is remove 'this'.
     return (
-      <div className="ui container">
+      <div className='ui container'>
         <header>
-          <h1 className="ui dividing header">React Animal Shelter</h1>
+          <h1 className='ui dividing header'>React Animal Shelter</h1>
         </header>
-        <div className="ui container">
-          <div className="ui grid">
-            <div className="four wide column">
-              <Filters />
+        <div className='ui container'>
+          <div className='ui grid'>
+            <div className='four wide column'>
+              <Filters handleClick={findPets} handleChange={changeFilter} />
             </div>
-            <div className="twelve wide column">
-              <PetBrowser />
+            <div className='twelve wide column'>
+              <PetBrowser pets={pets} adoptPet={adoptPet} />
             </div>
           </div>
         </div>
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,9 +3,36 @@ import React from 'react'
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
-  render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+  render () {
+    const { pets, adoptPet } = this.props
+    return <div className='ui cards'>
+      {
+        // For each pet pass a pet prop to the Pet component
+        pets.map(singlePet => <Pet key={singlePet.id} pet={singlePet} handleClick={() => adoptPet(singlePet.id)} />)
+      }
+    </div>
   }
 }
 
 export default PetBrowser
+
+/*
+
+refactored as functional Component:
+
+PetBrowser = (props) =>
+  <div className='ui cards'>
+      {
+        props.pets.map(singlePet => <Pet pet={singlePet} />)
+      }
+    </div>
+
+OR
+
+PetBrowser = ({ pets }) => // ES6 destructure just pets from the props
+  <div className='ui cards'>
+      {
+        pets.map(singlePet => <Pet pet={singlePet} />)
+      }
+    </div>
+*/
